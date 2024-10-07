fb4694a2f2ed2574edd9d2d06564ba00986dae2e
Brandon <brandon@huginn-noc.lan>
2024-10-06 19:51:07 -0500

    updates

---
+++ src/lib/hero.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Button from '$lib/button.svelte';
+</script>
+
+<div>
+	<img src="home-hero-min.jpg" alt="" />
+	<div id="grad"></div>
+	<div>
+		<h1></h1>
+		<span></span>
+		<Button></Button>
+	</div>
+</div>
--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -2,12 +2,12 @@
 	import Button from '$lib/button.svelte';
 </script>
 
-<div class="flex h-52 flex-row items-center justify-between px-[100px]">
+<div class="flex flex-row items-center justify-between lg:h-24 lg:px-12 2xl:h-52 2xl:px-28">
 	<div class="">
 		<!-- Add  here -->
-		<img src="logo.svg" class="w-[250px]" alt="Forbes Coastal Logo" />
+		<img src="logo.svg" class="lg:w-52 2xl:w-[250px]" alt="Forbes Coastal Logo" />
 	</div>
-	<div class="flex w-auto flex-row items-center gap-10">
+	<div class="hidden w-auto flex-row items-center gap-10 md:flex">
 		<a href="#" class="text-center">About</a>
 		<a href="#">Services</a>
 		<a href="#">Contact</a>
