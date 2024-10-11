9f5feac5b910ec9cb18de532d36690998fb56cdc
Brandon <brandon@huginn-noc.lan>
2024-10-11 18:35:39 -0500

    home progress

---
+++ src/lib/hero2.svelte
@@ -0,0 +1,28 @@
+<script>
+	// You can pass in props for the image, heading, and button text if needed
+	export let imageUrl;
+	export let heading = 'Create';
+</script>
+
+<div class="relative w-full">
+	<!-- Background Image -->
+	<div
+		class="h-[90vh] w-full bg-cover bg-center md:h-screen"
+		style="background-image: url({imageUrl});"
+	></div>
+
+	<!-- Gradient Overlay -->
+	<div class="absolute inset-0 bg-gradient-to-tl from-[#00194060] to-transparent"></div>
+
+	<!-- Content (Text + Button) -->
+	<div
+		class="absolute inset-0 flex items-center justify-center md:justify-start md:items-end md:p-10 text-sand"
+	>
+		<div class="max-w-2xl space-y-4 text-center md:text-left">
+			<h1 class="text-4xl font-bold md:text-6xl">{heading}</h1>
+
+			<!-- Button component can be imported and used here -->
+			<!-- <ButtonComponent text="Get Started" />-->
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
 	import NavBar from '$lib/navbar.svelte';
+	import Hero from '$lib/hero2.svelte';
 </script>
 
 <NavBar></NavBar>
-<!-- <Hero></Hero>
-<CenterText></CenterText>
+<Hero></Hero>
+<!-- <CenterText></CenterText>
 <ImageSplit></ImageSplit>
 <InstaFeed></InstaFeed>
 <Benefits></Benefits>
